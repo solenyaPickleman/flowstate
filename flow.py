c7f6bc73c9f2a6eef2e978a6a0aa481155cf9f84
user <user@pop-os.localdomain>
2022-09-03 15:45:24 +0100

    update for gpu machine"

--- flow.py
+++ flow.py
@@ -1,15 +1,28 @@
 '''
-pyspark --master local[6] --driver-memory 8g --jars /home/computron/postgresql-42.4.0.jar
+pyspark --master local[6] --driver-memory 8g --packages org.postgresql:postgresql:42.4.0 --packages com.johnsnowlabs.nlp:spark-nlp-gpu_2.12:4.1.0
+
+
+spark-submit --master local[6] --driver-memory 8g --packages org.postgresql:postgresql:42.4.0 flow.py 
 '''
 
 from flanker import mime 
 from flanker.addresslib import address  
 from dateutil import parser as dateparser 
 
+from pyspark import SparkContext 
+from pyspark.sql import SparkSession
 
 from pyspark.sql.functions import input_file_name, regexp_replace, trim, udf 
 from pyspark.sql.types import StructType, StringType, ArrayType , TimestampType
 
+
+spark = (
+    SparkSession
+    .builder
+    .appName("ETL email directory into postgres")
+    .getOrCreate())
+
+spark.sparkContext.setLogLevel('WARN')
 
 def parse_email (text : str ) -> dict : 
     #the element is the text of an email, MIME formatted 
@@ -37,7 +50,7 @@ dateformatter = udf ( lambda x : dateparser.parse(x) , TimestampType()) #.strfti
 emailformatter = udf ( lambda x : [address.strip() for address in x.split(',') ] if x is not None else None, ArrayType(StringType()))
 
 #read the data
-raw = spark.read.text('/home/computron/dataflows/enron/maildir/**/*', wholetext=True)
+raw = spark.read.text('/home/user/data/enron-maildir/**/*', wholetext=True).coalesce(240)
 #add the filename 
 raw = raw.withColumn("filepath", input_file_name())
 #parse out email specific fields into their own fields 
@@ -60,5 +73,5 @@ df = df.select(
     'x-filename'
 )
 
-df.write.jdbc(url='jdbc:postgresql://localhost:5432/computron', table='emails', mode='append', properties={'driver':'org.postgresql.Driver', 'user': 'computron','password': 'password'})
+df.write.jdbc(url='jdbc:postgresql://localhost:5432/emails', table='emails', mode='append', properties={'driver':'org.postgresql.Driver', 'user': 'flow','password': 'password'})
 
